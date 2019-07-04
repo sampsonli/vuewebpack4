24915e3b9e7161e858e7c7a977cac5e2a8db6a19
sampson Li <740056710@qq.com>
2019-07-04 11:00:38 +0800

    :+1:优化

--- src/routes/demo/pages/list/index.vue
+++ src/routes/demo/pages/list/index.vue
@@ -69,6 +69,8 @@
             },
             changeTitle () {
                 // model.mt.setAa('hello world')
+                console.log({ ...model.act })
+
                 model.act.getAa(Math.floor(Math.random() * 100))
             }
         },
